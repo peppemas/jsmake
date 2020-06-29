42e9623ccb65783bf5bbb0b393f69d2cd2f8df54
peppemas <giuseppe.mastrangelo@outlook.com>
2020-06-29 06:43:25 +0200

    bug fix: clear the buffer

--- src/bind_processor.hpp
+++ src/bind_processor.hpp
@@ -13,16 +13,13 @@ namespace ProcessorBinder {
 
 	class Process {
 	    private:
-	        char* m_output = 0;
-	        char* m_error = 0;
+	        std::string m_output;
+	        std::string m_error;
 		public:
 			explicit Process() {}
 
 			int Run(std::string str) 
 			{
-			    if (m_output) free(m_output);
-			    if (m_error) free(m_error);
-
 				std::vector<char *> args;
 				std::istringstream iss(str);
 
@@ -35,16 +32,19 @@ namespace ProcessorBinder {
 				}
 				args.push_back(0);
 
-                reproc_sink sinkOut = reproc_sink_string(&m_output);
-                reproc_sink sinkErr = reproc_sink_string(&m_error); // TODO: seems to not work...
+				char* output = NULL;
+                reproc_sink sinkOut = reproc_sink_string(&output);
 
-				int result = reproc_run_ex(&args[0], reproc_options{ 0 }, sinkOut, sinkErr);
+				int result = reproc_run_ex(&args[0], reproc_options{ 0 }, sinkOut, sinkOut);
 				if (result < 0) {
-					m_error = const_cast<char *>(reproc_strerror(result));
+				    m_error = std::string(reproc_strerror(result));
 					std::cout << "ERROR (" << result << "): " << m_error << std::endl;
 				} else {
-                    std::cout << m_output << std::endl;
+                    std::cout << output << std::endl;
 				}
+
+				if (output) m_output = std::string(output);
+                reproc_free(output);
 
                 for(size_t i = 0; i < args.size(); i++)
 					delete[] args[i];
@@ -54,18 +54,12 @@ namespace ProcessorBinder {
 
 			std::string GetOutput()
             {
-			    if (m_output != nullptr)
-			        return std::string(m_output);
-
-			    return 0;
+			    return m_output;
             }
 
             std::string GetError()
             {
-			    if (m_error != nullptr)
-			        return std::string(m_error);
-
-			    return 0;
+			    return m_error;
             }
 
 		    /**
